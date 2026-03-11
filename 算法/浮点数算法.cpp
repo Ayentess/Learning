d21a9c1774b3466d806e371b75bdf00dc33737ea
Ayentess <2479277792@qq.com>
2026-03-11 16:36:40 +0800

    提交

--- 算法/浮点数算法.cpp
+++ 算法/浮点数算法.cpp
@@ -159,3 +159,50 @@
 //	return 0;
 //}
 //
+
+
+//#include<iostream>
+//#include<string>
+//#include<algorithm>
+//#include<vector>
+//
+//using namespace std;
+//
+//vector<int> mul(vector<int>& A, int b)
+//{
+//    int t = 0;
+//    vector<int> C;
+//
+//    for (int i = 0; i < A.size()||t; i++)
+//    {
+//        if (i < A.size())
+//        {
+//            t += A[i] * b;
+//        }
+//        C.push_back(t % 10);
+//        t /= 10;
+//    }
+//
+//    while (1 < C.size() && C.back() == 0)C.pop_back();
+//    return C;
+//}
+//
+//int main()
+//{
+//    string a;
+//    int b;
+//
+//    vector<int>A;
+//
+//    cin >> a >> b;
+//    for (int i = a.size() - 1; i >= 0; i--)A.push_back(a[i] - '0');
+//
+//    auto C = mul(A, b);
+//
+//    for (int i = C.size() - 1; i >= 0; i--)
+//    {
+//        printf("%d", C[i]);
+//    }
+//    return 0;
+//}
+
